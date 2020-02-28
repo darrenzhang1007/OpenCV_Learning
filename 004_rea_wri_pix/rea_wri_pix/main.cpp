5fe4a408abf4fd955536ba75b16f6c43e8012fcc
DarrenmondZhang <785320051@qq.com>
2020-02-29 00:03:56 +0800

    day 004 commit

--- 004_rea_wri_pix/rea_wri_pix/main.cpp
+++ 004_rea_wri_pix/rea_wri_pix/main.cpp
@@ -17,20 +17,18 @@ int main(int argc, char** argv) {
 	int height = src.rows;
 	int width = src.cols;
 	int ch = src.channels();
-	for (int c = 0; c < ch; c++) {
-		for (int row = 0; row < height; row++) {
-			for (int col = 0; col < width; col++) {
-				if (ch == 3) {
-					Vec3b bgr = src.at<Vec3b>(row, col); // 获取3个字节类型的值，先行后列，
-					bgr[0] = 255 - bgr[0]; // 色彩取反 
-					bgr[1] = 255 - bgr[1];
-					bgr[2] = 255 - bgr[2];
-					src.at<Vec3b>(row, col) = bgr;
-				}
-				else if (ch == 1) {
-					int gray = src.at<uchar>(row, col);
-					src.at<uchar>(row, col) = 255 - gray;
-				}
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			if (ch == 3) {
+				Vec3b bgr = src.at<Vec3b>(row, col); // 获取3个字节类型的值，先行后列，
+				bgr[0] = 255 - bgr[0]; // 色彩取反 
+				bgr[1] = 255 - bgr[1];
+				bgr[2] = 255 - bgr[2];
+				src.at<Vec3b>(row, col) = bgr;
+			}
+			else if (ch == 1) {
+				int gray = src.at<uchar>(row, col);
+				src.at<uchar>(row, col) = 255 - gray;
 			}
 		}
 	}
@@ -40,24 +38,22 @@ int main(int argc, char** argv) {
 	Mat result = Mat::zeros(src.size(), src.type()); // 创建一个空白图像
 	int blue = 0, green = 0, red = 0;
 	int gray;
-	for (int c = 0; c < ch; c++) {
-		for (int row = 0; row < height; row++) {  
-			uchar* curr_row = src.ptr<uchar>(row); // 获取这一行所有像素点的像素块的首地址；这一行第一个像素的地址
-			uchar* result_row = result.ptr<uchar>(row);
-			for (int col = 0; col < width; col++) {
-				if (ch == 3) {
-					blue = *curr_row++;
-					green = *curr_row++;
-					red = *curr_row++;
+	for (int row = 0; row < height; row++) {  
+		uchar* curr_row = src.ptr<uchar>(row); // 获取这一行所有像素点的像素块的首地址；这一行第一个像素的地址
+		uchar* result_row = result.ptr<uchar>(row);
+		for (int col = 0; col < width; col++) {
+			if (ch == 3) {
+				blue = *curr_row++;
+				green = *curr_row++;
+				red = *curr_row++;
 
-					*result_row++ = blue;
-					*result_row++ = green;
-					*result_row++ = red;
-				}
-				else if (ch == 1) {
-					gray = *curr_row++;
-					*result_row++ = gray;
-				}
+				*result_row++ = blue;
+				*result_row++ = green;
+				*result_row++ = red;
+			}
+			else if (ch == 1) {
+				gray = *curr_row++;
+				*result_row++ = gray;
 			}
 		}
 	}
