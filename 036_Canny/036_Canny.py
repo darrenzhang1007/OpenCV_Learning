5179a6239bf127e00af128bdefbde57ab40fbd8b
DarrenmondZhang <785320051@qq.com>
2020-04-04 09:56:47 +0800

    day036_result commit

--- 036_Canny/036_Canny.py
+++ 036_Canny/036_Canny.py
@@ -8,7 +8,7 @@ cv.imshow("input", src)
 # t1 = 100, t2 = 3*t1 = 300
 edge = cv.Canny(src, 100, 300)
 cv.imshow("mask image", edge)
-cv.imwrite("D:/edge.png", edge)
+cv.imwrite("edge.png", edge)
 edge_src = cv.bitwise_and(src, src, mask=edge)
 
 h, w = src.shape[:2]
@@ -18,7 +18,7 @@ result[0:h,w:2*w,:] = edge_src
 cv.putText(result, "original image", (10, 30), cv.FONT_ITALIC, 1.0, (0, 0, 255), 2)
 cv.putText(result, "edge image", (w+10, 30), cv.FONT_ITALIC, 1.0, (0, 0, 255), 2)
 cv.imshow("edge detector", result)
-cv.imwrite("D:/result.png", result)
+cv.imwrite("result.png", result)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
