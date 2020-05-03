b1a4eaca6bcdeb15fd4b1cfec24e25c087ed8b1d
DarrenmondZhang <785320051@qq.com>
2020-05-03 11:28:03 +0800

    day054 55 56 commit

--- 054_fitEllipse/054_fitEllipse/054_fitEllipse.cpp
+++ 054_fitEllipse/054_fitEllipse/054_fitEllipse.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(int argc, const char *argv[])
 {
-	Mat src = imread("../test.jpg");
+	Mat src = imread("H:/OpenCV_Learning/054_fitEllipse/test.jpg");
 	if (src.empty()) {
 		printf("could not load image...\n");
 		return -1;
@@ -21,9 +21,7 @@ int main(int argc, const char *argv[])
 	imwrite("binary.png", binary);
 
 	Mat k = getStructuringElement(MORPH_RECT, Size(3, 3), Point(-1, -1));
-	cout << k << endl;
 	dilate(binary, binary, k);
-	imshow("dilate", binary);
 
 	// 轮廓发现与绘制
 	vector<vector<Point>> contours;
@@ -31,30 +29,11 @@ int main(int argc, const char *argv[])
 	findContours(binary, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point());
 	for (size_t t = 0; t < contours.size(); t++) {
 		// 最小外接轮廓
-		RotatedRect rrt = minAreaRect(contours[t]);
+		RotatedRect rrt = fitEllipse(contours[t]);
 		float w = rrt.size.width;
 		float h = rrt.size.height;
-		Point2f pts[4];
-		rrt.points(pts);
-
-		// 计算横纵比
-		float ratio = min(w, h) / max(w, h);
-		Point2f cpt = rrt.center;
-		circle(src, cpt, 2, Scalar(255, 0, 0), 2, 8, 0);
-		if (ratio > 0.9) {
-			circle(src, cpt, 2, Scalar(255, 0, 0), 2, 8, 0);
-			// 绘制旋转矩形与中心位置
-			for (int i = 0; i < 4; i++) {
-				line(src, pts[i % 4], pts[(i + 1) % 4], Scalar(0, 0, 255), 2, 8, 0);
-			}
-		}
-		if (ratio < 0.5) {
-			circle(src, cpt, 2, Scalar(255, 0, 0), 2, 8, 0);
-			// 绘制旋转矩形与中心位置
-			for (int i = 0; i < 4; i++) {
-				line(src, pts[i % 4], pts[(i + 1) % 4], Scalar(0, 255, 0), 2, 8, 0);
-			}
-		}
+		Point centers = rrt.center;
+		ellipse(src, rrt, Scalar(0, 0, 255), 2, 8);
 	}
 	imshow("contours", src);
 
